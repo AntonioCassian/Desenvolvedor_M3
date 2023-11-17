23914ea4d5decd5ba3da5e202d8387a968e3d90a
AntonioCassian <antoniomart849@gmail.com>
2023-11-17 09:13:10 -0300

    feat: create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
+import { Header } from './components/Header'
 import './styles/global.scss'
 
 function App() {
   return (
     <>
-      <div>
-        <h1>Desafio m3</h1>
-      </div>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import './styles.scss'
+import Logo from '../../assets/img/logo-m3.png'
+import { Icon } from '@iconify/react';
+
+export function Header() {
+    return (
+        <header>
+            <div className='container-nav'>
+                <img src={Logo} alt="Desenvolvedor-M3" />
+
+                <div className='bagde'>
+                    <Icon icon="game-icons:shopping-bag" className='icon' />
+                    <div>
+                        <span>1</span>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
